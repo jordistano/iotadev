19eba7552a73320ef3bdd6b1d1ce23a0e7f8c318
jordistano <jldistefano@gmail.com>
2024-02-25 14:00:53 -0300

    crud00

--- main.py
+++ main.py
@@ -1,38 +1,149 @@
-import flet as ft
+import sqlite3
+from flet import *
 
-def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    
+conn = sqlite3.connect("assets/mibase.db", check_same_thread=False)
+cursor=conn.cursor()
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
+
+
+
+def main(page:Page):
+    page.bgcolor="grey"
+    nametext=TextField(label="Name",color="cyan")
+    agetext=TextField(label="Age",color="cyan")
+    edit_nametext=TextField(label="Name",color="cyan")
+    edit_agetext=TextField(label="Age",color="cyan")
+    id_value=""
+    def deletebtn(e):
+        try:
+            cursor.execute("delete from user where id=?",str(e.control.data['id']))
+            conn.commit()
+            mydt.rows.clear()
+            load_data()
+            page.snack_bar=SnackBar(                
+                Text("Borrado!",size=20,text_align="CENTER"),
+                duration=1000,              
+                bgcolor="Red"
+                )
+            page.snack_bar.open=True
+            page.update()
+
+        except Exception as er:
+            print ("Error en borrado", er)
+        
+                           
+    def savedata(e):
+        try:
+            global id_value            
+            values=(edit_nametext.value,edit_agetext.value,str(id_value))            
+            cursor.execute("update user set name=?, edad=? where id=?",values)
+            conn.commit()
+            dialog.open=False
+            edit_agetext.value=""
+            edit_nametext.value=""            
+            id_value=""
+            mydt.rows.clear()
+            load_data()
+            page.snack_bar=SnackBar(
+                Text("Actualizado!",size=20,text_align="CENTER"),
+                bgcolor="green",
+                duration=1000)
+            page.snack_bar.open=True
+            page.update()
+
+
+        except Exception as er:
+            print(er,"error de update!")
+
+    dialog=AlertDialog(
+        title=Text("Edit data"),
+        content=Column([
+            edit_nametext,
+            edit_agetext
+        ]),
+        actions=[TextButton("Guardar",on_click=savedata)]
+    )    
+    def editbtn(e):
+        global id_value
+        edit_nametext.value=e.control.data["name"]
+        edit_agetext.value=e.control.data["edad"]
+        id_value=e.control.data["id"]
+        page.dialog=dialog
+        dialog.open=True
+        page.update()
+       
+    def load_data():
+        cursor.execute("select * from user")
+        results=cursor.fetchall()
+        columns=[column[0] for column in cursor.description]
+        rows=[dict(zip(columns,row))for row in results]
+        for row in rows:
+            mydt.rows.append(
+                DataRow(
+                    cells=[
+                        DataCell(Text(row["id"])),
+                        DataCell(Text(row["name"])),
+                        DataCell(Text(row["edad"])),
+                        DataCell(
+                            Row([
+                                IconButton("delete",icon_color="red",data=row,on_click=deletebtn),
+                                IconButton("create",icon_color="cyan",data=row,on_click=editbtn),
+
+                            ])
+                        ),
+                        
+                    ]
+                )
+            )
         page.update()
 
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
+
+    def addtodb(e):
+        try:
+            val=(nametext.value,agetext.value)
+            cursor.execute("INSERT INTO user (name,edad) VALUES(?,?)",val)
+            print(cursor.rowcount," inserted!!")
+            conn.commit()
+            mydt.rows.clear()
+            load_data()
+            page.snack_bar=SnackBar(
+                Text("Agregado!",size=20,text_align="CENTER"),
+                bgcolor="green",
+                duration=1000)
+            page.snack_bar.open=True
+            page.update()
+
+        except Exception as e:
+            print("error! ",e)
+        
+        nametext.value=""
+        agetext.value=""
         page.update()
 
-    page.add(
-        ft.Column([
-        ft.Row(
-            [
-                ft.Text("Plublicado desde Flet",size=30,color="green",bgcolor="pink"),
 
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    ])
+    mydt=DataTable(
+        columns=[
+            DataColumn(Text('id')),
+            DataColumn(Text('Name')),
+            DataColumn(Text('Age')),
+            DataColumn(Text('Controls')),
+        
+        ],
+        rows=[]
     )
 
-ft.app(target=main)
+    page.add(
+        Column([
+            nametext,
+            agetext,
+            ElevatedButton("Add data", on_click=addtodb),
+            mydt
+        ])
+    )
+
+    load_data()
+
+
+
+app(target=main, assets_dir="assets")
