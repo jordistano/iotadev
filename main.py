cd035eb24c5069c8713691d35bb724b077aa031d
jordistano <jldistefano@gmail.com>
2024-03-02 20:09:50 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-import flet as ft
-import flet_fastapi
-
-async def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    async def add_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        await counter.update_async()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.icons.ADD, on_click=add_click
-    )
-    await page.add_async(
-        ft.Container(counter, alignment=ft.alignment.center, expand=True)
-    )
-
-app = flet_fastapi.app(main)
+import uvicorn
+from os import getenv
+if __name__=="__main__":
+    port=int(getenv("PORT",8000))
+    uvicorn.run("app.api:app",host="0.0.0.0",port=port,reload=True)
