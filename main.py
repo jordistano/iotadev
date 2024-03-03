152d44de2a6da9f916c7e9d1885a4ef4f9d14354
jordistano <jldistefano@gmail.com>
2024-03-03 15:37:12 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+import flet as ft
+import flet_fastapi
 
-app = FastAPI()
+async def main(page: ft.Page):
+    counter = ft.Text("0", size=50, data=0)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello from FastAPI!"}
+    async def add_click(e):
+        counter.data += 1
+        counter.value = str(counter.data)
+        await counter.update_async()
 
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.icons.ADD, on_click=add_click
+    )
+    await page.add_async(
+        ft.Container(counter, alignment=ft.alignment.center, expand=True)
+    )
+ 
+
+app = flet_fastapi.app(main)
+@app.websocket("/app1/ws")
+async def flet_app(websocket: flet_fastapi.WebSocket):
+    await flet_fastapi.FletApp(main).handle(websocket) 
