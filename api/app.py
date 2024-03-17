7bdd9607dacdf954dfe35210a087cb983857966f
jordistano <jldistefano@gmail.com>
2024-03-16 23:56:37 -0300

    assets

--- api/app.py
+++ api/app.py
@@ -128,5 +128,5 @@ def main(page: Page):
   load_data()
 
 
-directorio=os.getcwd()+"/assets"
-app(target=main,view=AppView.WEB_BROWSER, assets_dir=str(directorio))
+#directorio=os.getcwd()+"/assets"
+app(target=main,view=AppView.WEB_BROWSER, assets_dir="assets")
